0ff1c9c8e944beeb7d9fb2bafdd92ae74caa2063
Daniel Novaes <heruying@gmail.com>
2017-04-10 17:25:19 -0300

    nothing important, just updating local repo

--- prepForMaratona/lpc-inf-ufrgs-aquecimento1/notasEMoedas.py
+++ prepForMaratona/lpc-inf-ufrgs-aquecimento1/notasEMoedas.py
@@ -1,18 +1,25 @@
 entrada = round(float(input()), 2);
 valores = [[100.00, 0], [50.00, 0], [20.00, 0], [10.00, 0], [5.00, 0], [2.00, 0], [1.00, 0], [0.50, 0], [0.25, 0], [0.10, 0], [0.05, 0], [0.01, 0]]
 i = 0;
-while (entrada >= 0.01):
+while (entrada >= 0.001):
+    entrada = round(entrada, 2);
     if ((entrada - valores[i][0]) >= 0):
-        entrada -= valores[i][0];
-        valores[i][1] += 1;
-        i += 1;
-    else:
-        i += 1;
+        valores[i][1] += (entrada // valores[i][0]);
+        entrada = (entrada % valores[i][0]);
+        print(entrada);
+    i += 1;
 
 i2 = 0;
-while (i2 <= i):
+while (i2 < i):
     if (valores[i2][1] > 0):
-        print("{} nota(s) de R$ {}".format(valores[i2][1], valores[i2][0]));
+        print("{} nota(s) de R$ {:.2f}".format(int(valores[i2][1]), valores[i2][0]));
+        i2 += 1;
+    else:
+        i2 += 1;
+
+while (i2 < i):
+    if (valores[i2][1] > 0):
+        print("{} moedas(s) de R$ {:.2f}".format(int(valores[i2][1]), valores[i2][0]));
         i2 += 1;
     else:
         i2 += 1;
