a983024a47d0e2397e2e8313038b5c15381c43a4
heruying@gmail.com <heruying@gmail.com>
2017-04-25 13:14:25 -0300

    creating a .gitignore and updating a file name

---
+++ algprog/studentsMean.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    float p1, p2, ap, tf, media, mediageral, melhormediafinal = 0;
+    int matricula, matriculamelhormedia, alunos, i;
+    printf("Quantos alunos terao suas notas calculadas?" );
+    scanf(" %d", &alunos);
+    for (i = 0; i < alunos; i++) {
+        printf("Insira os dados: ");
+        scanf("%d %d %d %d %d", &matricula, &p1, &p2, &ap, &tf);
+        media = (p1 + p2 + ap + tf) / 4;
+        mediageral += media;
+        if (media > melhormediafinal) {
+            melhormediafinal = media;
+            matriculamelhormedia = matricula;
+        }
+    }
+    printf("Media geral da turma: %f", (mediageral / alunos));
+    printf("Melhor media final de todos os alunos: %f, matrícula: %i", melhormediafinal, matriculamelhormedia);
+
+
+}
